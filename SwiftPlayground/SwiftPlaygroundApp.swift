0b9480ec0001265b79439b5e3e4e31fd2a20d339
Muchamad Iqbal Fauzi <fauzi@Muchamads-MacBook-Pro.local>
2025-08-04 21:41:12 +0700

    initial

---
+++ SwiftPlayground/SwiftPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPlaygroundApp.swift
+//  SwiftPlayground
+//
+//  Created by Muchamad Iqbal Fauzi on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
