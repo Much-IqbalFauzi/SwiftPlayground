9b2ba9da706b75ce3e1ae47807fdaf179a8abacf
Muchamad Iqbal Fauzi <iqbalfauzi.much@gmail.com>
2025-09-16 06:15:57 +0700

    step

---
+++ Swift/Intro.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphnie"
+//character = "Eloise"
+
+let actor = "Drew Barrymore"
+let quote = "I'm not weird, \"I'm\" a limited edition."
+let multiline = """
+Hello 
+World
+"""
+
+print(actor.count)
+print(actor.uppercased())
+print(quote.hasPrefix("I'm"))
+print(quote.hasSuffix("edition."))
+
+
+
+
